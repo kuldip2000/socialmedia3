df02a11af6dc9dacc66a7df1407eca88c1086f9f
kuldip2000 <157139786+kuldip2000@users.noreply.github.com>
2024-09-18 23:03:41 +0530

    changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,25 +13,25 @@ const Sidebar = () => {
         <svg className="bi pe-none me-2" width="40" height="32">
           <use xlinkHref="#bootstrap"></use>
         </svg>
-        <span className="fs-4">Sidebar</span>
+        <span className="fs-4 kkk">Sidebar</span>
       </a>
       <hr />
-      <ul className="nav nav-pills flex-column mb-auto home button">
+      <ul className="nav nav-pills flex-column mb-auto home button ">
         <li className="nav-item">
           <Link to="/" className="nav-link text-white" aria-current="page">
-            <svg className="bi pe-none me-2" width="16" height="16">
+            <svg className="bi pe-none me-2 " width="4%" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
           </Link>
         </li>
         <li>
-          <Link to="/create-post" className="nav-link text-white button">
+          <Link to="/create-post" className="nav-link text-white button ">
             <svg
-              className="bi pe-none me-2"
-              width="16"
+              className="bi pe-none me-2 "
+              width="2%"
               height="16"
-              margin-left="0px"
+              margin-right="0px"
             >
               <use xlinkHref="#speedometer2"></use>
             </svg>
@@ -40,41 +40,6 @@ const Sidebar = () => {
         </li>
       </ul>
       <hr />
-      <div className="dropdown">
-        <a
-          href="#"
-          className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <strong>Go Up</strong>
-        </a>
-        <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-          <li>
-            <a className="dropdown-item" href="#">
-              New project...
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Settings
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Profile
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
